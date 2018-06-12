eb45b30eb9caf0a8a55486180ac3cc4d1761ce1b
Wilson Uño <wils_cmb@hotmail.com>
2018-06-12 17:19:53 -0400

    cambios en el archivo app.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-var port = 3000;
+var port = 7777
 app.listen(port, () => {
-  console.log("servidor corriendo en" + port);
+  console.log("servidor corriendo en " + port);
 });
